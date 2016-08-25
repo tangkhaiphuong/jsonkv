72d336fc051f5e6e7a475ded377877e9219fb8e5
Tang Khai Phuong <tkphuong@omnicasa.com>
2016-08-25 10:24:03 +0700

    Update.

--- build/app.js
+++ build/app.js
@@ -1,28 +1,28 @@
 "use strict";
 const jsonkv_1 = require("./jsonkv");
 const root = require("./generated");
-// console.log("\r\nJSON object: ");
-// console.log(root);
-// let kv = toKv(root);
-// console.log("\r\nConvert to plain key-value: ");
-// console.log(kv);
-// let json = toJson(kv);
-// console.log("\r\nConvert back to json: ");
-// console.log(json);
-//Testing performance.
-let jsonString = JSON.stringify(root);
-let start = Date.now();
-for (let i = 0; i < 100000; ++i) {
-    JSON.parse(jsonString);
-}
-let end = Date.now();
-console.log("Time to JSON.parse: " + (end - start));
-let keyValue = jsonkv_1.toKv(root);
-let start2 = Date.now();
-for (let i = 0; i < 100000; ++i) {
-    jsonkv_1.toJson(keyValue);
-}
-let end2 = Date.now();
-console.log("Time to toJSON: " + (end2 - start2));
+console.log("\r\nJSON object: ");
+console.log(root);
+let kv = jsonkv_1.toKv(root);
+console.log("\r\nConvert to plain key-value: ");
+console.log(kv);
+let json = jsonkv_1.toJson(kv);
+console.log("\r\nConvert back to json: ");
+console.log(json);
+// Testing performance.
+// let jsonString = JSON.stringify(root);
+// let start = Date.now();
+// for (let i = 0; i < 100000; ++i) {
+//     JSON.parse(jsonString);
+// }
+// let end = Date.now();
+// console.log("Time to JSON.parse: " + (end - start));
+// let keyValue = toKv(root);
+// let start2 = Date.now();
+// for (let i = 0; i < 100000; ++i) {
+//     toJson(keyValue);
+// }
+// let end2 = Date.now();
+// console.log("Time to toJSON: " + (end2 - start2));
 
 //# sourceMappingURL=app.js.map
